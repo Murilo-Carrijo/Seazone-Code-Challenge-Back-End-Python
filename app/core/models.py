8eac8ddf975ee98265c2131694c8e2d37ceae6e0
murilo <murilocarrijoadm@gmail.com>
2022-11-28 18:52:26 -0300

    feat: Implementando a model para os Imóveis

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,16 @@
-from django.db import models # noqa
+from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Properties(models.Model):
+    """Imóveis object."""
+    title = models.CharField(max_length=255)
+    max_people = models.IntegerField()
+    qty_bathrooms = models.IntegerField()
+    pet_frendly = models.BooleanField()
+    cleaning_value = models.DecimalField(max_digits=5, decimal_places=2)
+    activation_date = models.DateTimeField(auto_now_add=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
