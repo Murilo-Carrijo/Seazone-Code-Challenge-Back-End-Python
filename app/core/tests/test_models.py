d20eeb4632ffbdda2b0f74b0ee3081c0afc15c23
murilo <murilocarrijoadm@gmail.com>
2022-11-28 18:32:56 -0300

    test: implementando teste para a model referente ao cadastro de imóveis

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,23 @@
+"""
+Testes para as models.
+"""
+from decimal import Decimal
+
+from django.test import TestCase
+
+from core import models
+
+class ModelTests(TestCase):
+    """Teste das MOodels"""
+
+    def test_create_properties(self):
+        """Teste para adicionar um novo imóvel com sucesso"""
+
+        properties = models.Properties.objects.create(
+            max_people=10,
+            qty_bathrooms=2,
+            pet_frendly=True,
+            cleaning_value=Decimal('50.50'),
+        )
+
+        self.assertEqual(int(properties), properties.max_people)
