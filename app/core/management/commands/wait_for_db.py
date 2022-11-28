4c180c6e4ff1825f364827dd8db3af3038df7960
murilo <murilocarrijoadm@gmail.com>
2022-11-28 11:22:33 -0300

    feat: Implementando a função para aguradar o banco de dados iniciar

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,11 +1,28 @@
 """
 Comando Django para aguardar a inicialização do banco de dados.
 """
+import time
 
+from psycopg2 import OperationalError as Psycopg2OpError
+
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
+
 
 class Command(BaseCommand):
     """Comando Django para aguardar o banco de dados"""
 
     def handle(self, *args, **options):
-        pass
+        self.stdout.write('Aguardando o banco de dados...')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write(
+                    'O banco de dados esta indisponivel, aguarde 1 segundo...'
+                )
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('Banco de dados disponível!'))
