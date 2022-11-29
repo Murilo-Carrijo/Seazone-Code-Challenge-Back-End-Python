af2bb999024a9867fa9318b56426a78c5ddb12a3
murilo <murilocarrijoadm@gmail.com>
2022-11-29 18:31:29 -0300

    feat: Implementando a view para os anúncios

--- app/properties/views.py
+++ app/properties/views.py
@@ -3,8 +3,15 @@ Views para as APIs de Imóveis
 """
 from rest_framework import viewsets
 
-from core.models import Properties
-from .serializers import PropertiesSerializer, PropertyDetailSerializer
+from core.models import (
+    Properties,
+    Advertisement
+)
+from .serializers import (
+    PropertiesSerializer,
+    PropertyDetailSerializer,
+    AdvertisementSerializer,
+)
 
 
 class PropertiesViewSet(viewsets.ModelViewSet):
@@ -18,3 +25,17 @@ class PropertiesViewSet(viewsets.ModelViewSet):
             return PropertiesSerializer
 
         return self.serializer_class
+
+
+class AdvertisementViewSet(viewsets.ModelViewSet):
+    """View para gerenciar as APIs de anúncios."""
+    queryset = Advertisement.objects.all().order_by('-id')
+    serializer_class = AdvertisementSerializer
+
+    def get_serializer_class(self):
+        """Retorna a classe serializer via request."""
+        if self.action == 'list':
+            return AdvertisementSerializer
+
+        return self.serializer_class
+
