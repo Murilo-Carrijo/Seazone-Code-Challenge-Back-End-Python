b57e9891eb94682640c5b574226e64b2e2735f89
murilo <murilocarrijoadm@gmail.com>
2022-11-29 17:49:56 -0300

    chore: Registrando a rota para os Anúncios

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
-from .models import Properties
+from .models import (
+    Properties,
+    Advertisement,
+)
 
 admin.site.register(Properties)
+admin.site.register(Advertisement)
